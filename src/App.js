716994246cb7337707f102e452eebb5afc091db3
jgomez002 <jdg8858@nyu.edu>
2022-09-20 09:46:24 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  createBrowserRouter,RouterProvider, Route,} from "react-router-dom";
+import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <List />,
+  },
+  {
+    path: "/article",
+    element: <Article />,
+  }
+]);
+
+import List from "./pages/list";
+
+import Article from "./pages/article";
 
 function App() {
-  const coolVarible = "cool"
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>(coolVarible)</h1>
-        <p>This updates lol</p>
-      </header>
+      <List/>
     </div>
   );
 }
