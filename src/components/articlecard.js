043a4c3302ebe3f0ec0d20f6cf78458c6de0f148
jgomez002 <jdg8858@nyu.edu>
2022-09-27 08:44:40 -0400

    update w/json

--- src/components/articlecard.js
+++ src/components/articlecard.js
@@ -7,9 +7,9 @@ function ArticleCard({description, date, imageAlt, imageSrc, title}){
                 <img src={imageSrc} alt={imageAlt}/>
             </div>
             <div className="articleCard--text">
-            <h2>Article Title</h2>
-                <p className="date">Wednesday, August 22, 2019</p>
-                <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h2>{title}</h2>
+                <p className="date">{date}</p>
+                <p className="description"> {description}</p>
                 <p className="articleCard--link"> <a href="/article">Read More</a> </p>
             </div>
         </div>
--- src/pages/list.js
+++ src/pages/list.js
@@ -1,17 +1,22 @@
 import React from "react";
 import ArticleCard from "../components/articlecard";
+import Data from "../components/data"
 
 
 function List(){
     return(
         <main className="page-wrapper">
             <h1>Articles</h1>
-            <ArticleCard 
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            date = "04/02/2003"
-            title= "articleTitle"/>
-            <ArticleCard />
-            <ArticleCard />
+            {Data.map((article,i) => (
+                 <ArticleCard 
+                 key = {i}
+                 description = {article.blurb}
+                 date = {article.publishedDate}
+                 title= {article.title}
+                 imageAlt ={article.image.alt}
+                 imageSrc= {article.image.src}
+                 />
+            ))}
         </main>
     );
 }
